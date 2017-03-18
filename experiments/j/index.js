9fea197c6ae3c6ae5507c4bed00f819c3358008a
Bruno Torres <me@brunotorr.es>
2017-03-18 16:44:19 -0300

    feat(app detection): initial version

---
+++ experiments/j/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('.app-list li').click(function() {
+    var timeoutId = this.checkTimeout
+    clearTimeout(timeoutId)
+
+    var iframe = $('<iframe />')
+
+    var clearIframe = function() {
+      iframe.remove()
+    }
+
+    var $el = $(this)
+    iframe.on('load', function() {
+      console.log('loaded!')
+      clearTimeout(timeoutId)
+      clearIframe()
+      $el.find('.result').text('Installed')
+    })
+
+    iframe.attr('src', 'waze://?ll=0.0')
+    $el.append(iframe)
+    timeoutId = this.checkTimeout = setTimeout(function() {
+      clearIframe()
+      $el.find('.result').text('Not Installed')
+    }, 2000)
+  })
+})
